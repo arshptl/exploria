103a013021453a77e19e246d4d0df95d8ad867d8
arshptl <arshpatel714@gmail.com>
2022-11-18 20:36:39 +0530

    misse: commented code

--- server/src/db/User.js
+++ server/src/db/User.js
@@ -3,7 +3,13 @@ const { model, Schema } = require("mongoose");
 const UserSchema = new Schema({
         name: String,
         email: String,
-        password: String
+        password: String,
+        // history: [
+        //         {
+        //                 type: Schema.Types.ObjectId,
+        //                 ref: "Places"
+        //         }
+        // ]
 });
 
 module.exports = model('User', UserSchema);
